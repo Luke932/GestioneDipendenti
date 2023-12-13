e3eae1c918d0a9a213a6defc7b17a50391dfc8bd
enrik-rucaj <43344630+enrik-rucaj@users.noreply.github.com>
2023-12-13 15:41:01 +0100

    frontend lista orari

--- GestioneDipendenti-Frontend/src/app/components/homeutente/homeutente.component.ts
+++ GestioneDipendenti-Frontend/src/app/components/homeutente/homeutente.component.ts
@@ -9,6 +9,7 @@ import { UtentiService } from "src/app/services/utenti.service";
 export class HomeutenteComponent implements OnInit {
   isIngEnabled = true;
   isExitEnabled = false;
+  enterFor = true;
   username: any;
   id: any = localStorage.getItem("id");
   array: any= [];
@@ -24,7 +25,10 @@ export class HomeutenteComponent implements OnInit {
 
   ngOnInit(): void {
     this.username = localStorage.getItem("username");
+    console.log(this.enterFor + "!!!!!!!!!!!!!!!!!!!!!");
+    
     this.getUtenteById();
+    //this.enterFor = false;
   }
 
   clickEntrata() {
@@ -32,6 +36,7 @@ export class HomeutenteComponent implements OnInit {
 
     this.isIngEnabled = false;
     this.isExitEnabled = true;
+    this.enterFor = false;
     this.utentiSrv.dataIngresso(this.id).subscribe(
       (response) => {
         console.log("Data entrata salvata con successo", response);
@@ -60,13 +65,39 @@ export class HomeutenteComponent implements OnInit {
   getUtenteById() {
     this.utentiSrv.getUtenteById(this.id).subscribe(
       (response) => {
+        this.data = null;
+        this.orarioIngresso = null;
+        this.orarioUscita = null;
+
         this.utente = response;
         console.log(this.utente);
-        console.log(this.utente.orari[0].dataIngresso);
+        //console.log(this.utente.orari[0].dataIngresso);
         this.dataOrario = this.utente.orari;
         console.log(this.dataOrario);
-        for (let i = 0; i < this.utente.orari.length; i++) {
-          const orarioCorrente = this.utente.orari[i];
+
+        console.log(this.enterFor + "!!!!!!!!!!!coooooooosaaaa");
+        
+        if(this.enterFor == true) {
+          console.log("sono entratoooooooo!!!!!!");
+          
+          for (let i = 0; i < this.utente.orari.length; i++) {
+            const orarioCorrente = this.utente.orari[i];
+            this.data = orarioCorrente.dataIngresso.substring(0, 10);
+            this.orarioIngresso = orarioCorrente.dataIngresso.substring(11, 19);
+            this.orarioUscita = orarioCorrente.dataUscita.substring(11, 19);
+            this.dataOrario =
+            {
+              data: this.data,
+              ingresso: this.orarioIngresso,
+              uscita: this.orarioUscita
+            };
+            this.array.push(this.dataOrario);
+          }
+        }
+
+        //for (let i = 0; i < this.utente.orari.length; i++) {
+          //const orarioCorrente = this.utente.orari[i];
+          const orarioCorrente = this.utente.orari[this.utente.orari.length - 1];
 
           this.data = orarioCorrente.dataIngresso.substring(0, 10);
           console.log(this.data);
@@ -74,19 +105,29 @@ export class HomeutenteComponent implements OnInit {
           this.orarioIngresso = orarioCorrente.dataIngresso.substring(11, 19);
           console.log(this.orarioIngresso);
 
-          this.orarioUscita = orarioCorrente.dataUscita.substring(11, 19);
+          if (this.isExitEnabled == false) {
+            this.orarioUscita = orarioCorrente.dataUscita.substring(11, 19);
+          }
           console.log(this.orarioUscita);
-            this.dataOrario =
-            {
-              data: this.data,
-              ingresso: this.orarioIngresso,
-              uscita: this.orarioUscita
-            };
-          this.array.push(this.dataOrario);
+          this.dataOrario =
+          {
+            data: this.data,
+            ingresso: this.orarioIngresso,
+            uscita: this.orarioUscita
+          };
+          
+          if (this.isIngEnabled == false) {
+            this.array.push(this.dataOrario);
+          }
+          else {
+            this.array[this.array.length - 1] = this.dataOrario;
+          }
+          //this.array.push(this.dataOrario);
           console.log(this.array);
 
-        }
+        //}
 
+        //non serve in teoria
         console.log(response);
         this.utente = response;
       },
